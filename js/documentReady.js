d9da3cf310f6b3d3e253c98d76bbe13b4a123cbc
trungducng <caoboiyb1997@gmail.com>
2018-01-20 09:56:52 +0700

    Update 10h

---
+++ js/documentReady.js
@@ -0,0 +1,15 @@
+$(document).ready(() => {
+    if (sessionStorage.userID != null){
+        document.getElementById("signin").innerHTML = "Profile";
+        document.getElementById("signin").setAttribute("href","/edit-profile");
+
+        document.getElementById("signup").innerHTML = "Logout";
+        document.getElementById("signup").setAttribute("href", "/")
+        document.getElementById("signup").classList.add("logoutButton")
+    }
+})
+
+$(".logoutButton").on("click", event => {
+    sessionStorage.userID = null;
+})
+
